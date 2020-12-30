e18e548c9c54c4348f4642339a2d1a049d36f88e
Nicholas McClain-Simpson <n.mcsimp@gmail.com>
2020-12-30 09:34:41 -0600

    Make the Form Reactive

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+// import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +10,50 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // Create instance when form is initialized
+    // Manual Form Model declaration
+    // this.customerForm = new FormGroup({
+    //   //add form controls/groups
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true),
+    // });
+
+    // Using FormBuilder
+    this.customerForm = this.fb.group({
+      firstName: "",
+      lastName: {value: 'n/a', disabled: false},
+      email: "",
+      sendCatalog: {value: true, disabled: false},// Get more fancy
+    });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void {
+    this.customerForm.setValue({// Must set all values
+      firstName: "Jack",
+      lastName: "Harkness",
+      email: "jackHarkness@gmail.com",
+      sendCatalog: false,
+    });
+
+    // Use patchValue for a subset
   }
+
+  save(): void {
+      console.log(this.customerForm);
+      console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+  // Old template save:
+  // save(customerForm: NgForm): void {
+  //   console.log(customerForm.form);
+  //   console.log('Saved: ' + JSON.stringify(customerForm.value));
+  // }
 }
